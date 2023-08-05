db485fcaa9ca6276806bb4c90e0291b10982a7b4
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2023-08-05 15:10:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let ss = ("hello world")
 console.log (ss)
-alert ("it is working");
+//alert ("it is working");
 console.log ("it is working");
 alert ("now it is working let me se if i can update it in real time from here");
