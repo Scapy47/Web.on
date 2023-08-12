85f3adc109b4ca240880d87e7e3405d095911f8a
sajalsafalsharma1234@gmail.com <sajalsafalsharma1234@gmail.com>
2023-08-12 19:28:32 +0530

    i used / in html file which was causing problem now it's fixed and now i am trying new server worker by bard i will rewrite it after learn JavaScript

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,50 @@
-// Install the service worker and cache essential assets
+// The service worker file should be named `serviceworker.js`
+
+// Import the ServiceWorkerRegistration API
+import { ServiceWorkerRegistration } from 'serviceworker-registration';
+
+// Define the service worker
+const sw = new ServiceWorkerRegistration('serviceworker.js');
+
+// Listen for the install event
+sw.addEventListener('install', async (event) => {
+  // Cache all the necessary files for offline usage
+  const cache = await sw.caches.open('my-pwa-cache');
+  await cache.addAll([
+    'index.html',
+    'styles.css',
+    'scripts.js',
+    'images/logo.png',
+  ]);
+});
+
+// Listen for the activate event
+sw.addEventListener('activate', (event) => {
+  // Clear the old cache
+  event.waitUntil(sw.caches.delete('old-cache'));
+});
+
+// Listen for the fetch event
+sw.addEventListener('fetch', async (event) => {
+  // Check if the request is for a cached file
+  const cachedResponse = await sw.caches.match(event.request);
+
+  // If the request is for a cached file, return the cached response
+  if (cachedResponse) {
+    event.respondWith(cachedResponse);
+  } else {
+    // If the request is not for a cached file, fetch the resource from the network
+    event.respondWith(await fetch(event.request));
+  }
+});
+
+
+
+
+
+
+
+/*// Install the service worker and cache essential assets
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('your-app-cache').then(cache => {
@@ -20,4 +66,4 @@ self.addEventListener('fetch', event => {
       return response || fetch(event.request);
     })
   );
-});
+});*/
